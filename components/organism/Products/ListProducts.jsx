c403be9e5ae3b89992ffd12834612e12ed199432
lordgent <lorjenx08@gmail.com>
2022-01-07 21:38:05 +0700

    create dummy data products

--- components/organism/Products/ListProducts.jsx
+++ components/organism/Products/ListProducts.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import products from "../../../data/products.json";
 function ListProducts() {
   return (
     <div className="w-full px-8">
@@ -10,6 +10,15 @@ function ListProducts() {
         here are many variations of passages of Lorem Ipsum available, but the
         majority have suffered alteration in some
       </p>
+      <div className="grid grid-cols-6 gap-2">
+        {products?.map((item) => (
+          <div key={item.id}>
+            <div>
+              <p>{item?.name_product}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
